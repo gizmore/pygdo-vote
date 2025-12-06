53e89dd6969af844349c42d3bfd22b9715537560
gizmore <gizmore@wechall.net>
2025-12-06 19:04:00 +0100

    'sd'

--- GDT_VoteCount.py
+++ GDT_VoteCount.py
@@ -3,16 +3,26 @@ from gdo.base.Trans import t
 from gdo.core.GDT_UInt import GDT_UInt
 
 from typing import TYPE_CHECKING
+
+from gdo.vote.GDO_VoteTable import GDO_VoteTable
+
 if TYPE_CHECKING:
     from gdo.vote.WithVotes import WithVotes
 
 
 class GDT_VoteCount(GDT_UInt):
 
+    _table: GDO_VoteTable|None
+
     def __init__(self, name: str):
         super().__init__(name)
+        self._table = None
         self.not_null()
         self.initial('0')
+
+    def table(self, table: GDO_VoteTable):
+        self._table = table
+        return self
 
     def gdo_with_votes(self) -> 'GDO|WithVotes':
         return self._gdo
--- GDT_VoteResult.py
+++ GDT_VoteResult.py
@@ -1,8 +1,15 @@
 from gdo.core.GDT_Decimal import GDT_Decimal
+from gdo.vote.GDO_VoteTable import GDO_VoteTable
 
 
 class GDT_VoteResult(GDT_Decimal):
 
+    _table: GDO_VoteTable
+
     def __init__(self, name: str):
         super().__init__(name)
         self.digits(2, 1)
+
+    def table(self, table: GDO_VoteTable):
+        self._table = table
+        return self
